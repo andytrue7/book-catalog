39a3a2a2e331f97bc341a941fc4f4d7e1cc014da
Andrey <univer1857@gmail.com>
2020-09-30 13:02:18 +0300

    add main page and single book page

---
+++ app/Http/Controllers/CatalogController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repository\BookRepository;
+use Illuminate\Http\Request;
+
+class CatalogController extends Controller
+{
+    private $bookRepository;
+
+    public function __construct(BookRepository  $bookRepository)
+    {
+        $this->bookRepository = $bookRepository;
+    }
+
+    public function index()
+    {
+        $books = $this->bookRepository->findLatest();
+        return view('catalog.catalog', compact('books'));
+    }
+
+    public function showBookPage($id)
+    {
+        $book = $this->bookRepository->findById($id);
+
+        return view('catalog.show', compact('book'));
+    }
+}
